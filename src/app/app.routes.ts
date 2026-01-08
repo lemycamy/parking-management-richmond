63b37f35494cf2fd478c7ae2f17c6aef142e89b2
lemycamy <lyle.damien17.com>
2026-01-09 04:11:05 +0800

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Parking } from './features/parking/parking';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 
+        path: 'parking', 
+        component: Parking
+    },
+];
