005207143a29a938d8ebcb349124e23c320caa7f
Ace <aceonsteam001@gmail.com>
2026-01-23 08:17:39 +0800

    add: brand logo, fix: prod error

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://lease-linq-backend.up.railway.app/api/v2/graphql',
+  apiBaseUrl: 'https://lease-linq-backend.up.railway.app/graphql',
 };
